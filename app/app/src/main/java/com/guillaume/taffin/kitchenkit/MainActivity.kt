8de28dadfe2366ce31edb8a9be9aa9b518f8ea13
guillaumetaffin <Guillaume.Taffin@opal-rt.com>
2020-10-03 17:39:39 -0400

    create app project

---
+++ app/app/src/main/java/com/guillaume/taffin/kitchenkit/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.guillaume.taffin.kitchenkit
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
